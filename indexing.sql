3e1df89257c62914eec18f7ddef38e3c88ae4f7d
Dakota <bazookaharwell@gmail.com>
2017-04-25 15:27:20 -1000

    Create query 2

--- indexing.sql
+++ indexing.sql
@@ -1,6 +1,12 @@
-\timing
+
 
 --1.
 SELECT DISTINCT make_title
 FROM car_models
 WHERE make_code = 'LAM';
+
+--2.
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code = 'NISSAN'
+  AND model_code = 'GT-R';
